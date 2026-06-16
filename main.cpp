b5c145f087f89efd0e1ca934b1ff5f087bf62a6a
Jose Luna <josemanuelmluna@gmail.com>
2026-06-16 12:52:00 +0800

    feat: produced an LBlock on screen

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
+#include "classes/colors.h"
 #include "classes/grid.h"
 #include "raylib.h"
 #include <cstddef>
+#include "classes/blocks.cpp"
 
 Grid grid = Grid();
 
 int main()
 {
-    Color darkBlue {44, 44, 127, 255};
     InitWindow(300, 600, "Tetris");
     SetTargetFPS(60);
+
+    grid.Print();
+
+    LBlock lblock = LBlock();
+
     while (WindowShouldClose() == false) 
     {
 
         BeginDrawing();
-        ClearBackground(darkBlue);
+        ClearBackground(GetCellColor(CellType::DarkBlue));
         grid.Draw();
+        lblock.Draw();
         EndDrawing();
     }
 
